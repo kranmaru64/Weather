b248aeba14570b8cc7e7c0e34729b6ea32ffa9fe
kranmaru64 <135051488+kranmaru64@users.noreply.github.com>
2023-09-07 15:58:50 -0700

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    // weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather?city=%3CREQUIRED%3E',
+    weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather?',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '78f0cd8721msh0d82e4762f898bep1190cfjsn998f5fbfc14d',
+};
